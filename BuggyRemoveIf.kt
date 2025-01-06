f5caef9d67c22441358bdd0202688e78165c4dee
BugHunter-X <askzenai@gmail.com>
2025-01-06 13:00:06 +0100

    Add BuggyRemoveIf.kt file

---
+++ BuggyRemoveIf.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val list = mutableListOf(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+
+    val set = mutableSetOf(1, 2, 3, 4, 5)
+    set.removeIf { it > 2 }
+    println(set) // Output: [1, 2]
+
+    val map = mutableMapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five")
+    map.removeIf { it.key > 2 }
+    println(map) // Output: {1=one, 2=two}
+}
